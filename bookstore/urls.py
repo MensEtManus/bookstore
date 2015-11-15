cab178accac78c6b4d083450055d7424e50a07c8
Albert Yang <ayoungbest@gmail.com>
2015-11-14 21:34:35 -0500

    installed registration added templates for login and requirements

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [ 
@@ -7,5 +7,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^store/', include('store.urls'), name='store'),
+		url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
