49fdc09a0d1ecc385011ef1ec834cf5cce06f8f3
Albert Yang <ayoungbest@gmail.com>
2015-11-20 18:11:45 -0500

    fixed registration email confirmation; add models, views, templates for shopping cart

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Book, Author
+from .models import Book, Author, BookOrder, Cart
 
 class BookAdmin(admin.ModelAdmin):
 	list_display = ('title', 'author', 'price', 'stock')
@@ -9,5 +9,14 @@ class BookAdmin(admin.ModelAdmin):
 class AuthorAdmin(admin.ModelAdmin):
 	list_display = ('last_name', 'first_name')
 
+class BookOrderAdmin(admin.ModelAdmin):
+	list_display = ('book', 'cart', 'quantity') 
+
+class CartAdmin(admin.ModelAdmin):
+	list_display = ('user', 'active', 'order_date') 
+
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author, AuthorAdmin)
+admin.site.register(BookOrder, BookOrderAdmin)
+admin.site.register(Cart, CartAdmin)
+
